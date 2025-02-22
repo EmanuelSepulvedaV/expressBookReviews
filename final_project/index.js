915a91fb90f254d11aad4365785793742bf7c9ff
KROSZ2BleAA <128820420+EmanuelSepulvedaV@users.noreply.github.com>
2025-02-22 13:55:19 -0500

    commit

--- final_project/index.js
+++ final_project/index.js
@@ -18,14 +18,12 @@ app.use(
 );
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-  // Check if user is logged in and has valid access token
   if (req.session.authorization) {
     let token = req.session.authorization["accessToken"];
-    // Verify JWT token
     jwt.verify(token, "access", (err, user) => {
       if (!err) {
         req.user = user;
-        next(); // Proceed to the next middleware
+        next();
       } else {
         return res.status(403).json({ message: "User not authenticated" });
       }
